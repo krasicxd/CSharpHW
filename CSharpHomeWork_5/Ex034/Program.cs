faeeb9d091685989c9a5522ec2f255b1dcc1ff0c
Daniil S <daniil.2210s@gmail.com>
2023-10-02 13:40:18 +0300

    Array comma fixed

---
+++ CSharpHomeWork_5/Ex034/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArray()
+{
+    int size = new Random().Next(1,6);
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,999);
+    };
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {     
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+void EvenCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
+}
+
+Console.WriteLine();
+
+int [] newArray = CreateArray();
+
+Console.Write("Out of: ");
+
+PrintArray(newArray);
+
+Console.WriteLine();
+
+Console.Write("Quantity of even numbers is: ");
+
+EvenCount(newArray);
+
+Console.WriteLine();
+
+
+
+
