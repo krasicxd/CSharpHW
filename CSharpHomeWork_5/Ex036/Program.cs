e73e3cfd92a6bc6fafb1075aba84f0247b9c0846
Daniil S <daniil.2210s@gmail.com>
2023-10-02 14:30:39 +0300

    HW 5 completed

--- CSharpHomeWork_5/Ex036/Program.cs
+++ CSharpHomeWork_5/Ex036/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+
+int[] CreateArray()
+{
+    int size = new Random().Next(1,10);
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100,100);
+    };
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {     
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+void OddSumm(int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            summ = summ + array[i];
+        }
+    }
+    Console.WriteLine(summ);
+}
+
+Console.WriteLine();
+
+int [] newArray = CreateArray();
+
+Console.Write("Out of: ");
+
+PrintArray(newArray);
+
+Console.WriteLine();
+
+Console.Write("Summ for the odd nums is: ");
+
+OddSumm(newArray);
+
+Console.WriteLine();
+
+
+
+
--- CSharpHomeWork_5/Ex038/Program.cs
+++ CSharpHomeWork_5/Ex038/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] array = {3.22, 4.2, 1.15, 77.15, 65.2};
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {     
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+void SortArray(double[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        double temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+double GetMinMax(double[] array)
+{
+    double min = array[0];
+    double max = array[4];
+    double result = max - min;
+    return result;
+}
+
+Console.WriteLine();
+Console.Write("Within an array: ");
+PrintArray(array);
+Console.WriteLine();
+
+Console.Write("Max and min elements found: ");
+SortArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+Console.Write("The difference between elements is: ");
+Console.WriteLine(GetMinMax(array));
+Console.WriteLine();
+
