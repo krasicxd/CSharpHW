487a4bbf94289decdbe60ac538bcf2ee2fb8a2ba
Daniil S <daniil.2210s@gmail.com>
2023-10-09 11:54:13 +0300

    HW 6 added

---
+++ CSharpHomeWork_6/Ex041/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int NumbersAbove0Count(int [] array)
+{
+   int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Type your numbers: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+Console.WriteLine($"There are {NumbersAbove0Count(array)} numbers above zero");
+
+
